2ddf402e66d4050c168b1890183e5aabb3793e3b
evoeges <evoeges@gmail.com>
2019-02-09 19:04:17 -0500

    added definitions toggle

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 
 function toggleDefinitions(inputSwitch) {
-  console.log(inputSwitch)
+
+
   var x = document.getElementById("definitions");
   if (inputSwitch === "plain") {
-    x.innerHTML = "an easier definition!";
+    x.innerHTML = "An " + "<em>" + "automated decision system" + "</em>" +  " (ADS) is any decision a city government makes that is in some way helped by a computer. Often the decision is used to guide a human decision." + "<br>" + "<br>" +
+    "For example, the city might use a computer program to decide where the best location for a new park is. Or a city might use a computer program to decide where a child from a certain neighborhood should go to school." + "<br>" + "<br>" +
+    "These decisions can have far reaching consequences for people, sometimes good or bad.";
   } else {
-    x.innerHTML = "an easier definition!!!";
+    x.innerHTML = "An " + "<em>" + "automated decision system" + "</em>" +  " (ADS) refers to any set of formal or informal processes used by a city agency or governmental body to make, or assist in making, decisions; which" +
+      "<ul>" +
+        "<li>"+  "relies on one or more components that are, partially or entirely, computerized; which" + "</li>" +
+        "<ul>" +
+          "<li>" + "play a material role in the decision-making process; and where" + "</li>" +
+          "<li>" +  "the decision being made has a matieral impact on an affected individual or population."  + "</li>" +
+        "</ul>" +
+      "</ul>" +
+      "An ADS may either be used to replicate a human decision or process, or establish a new process entirely. <strong> An ADS must have at least one computerized component that plays a material role in making or meaningfully influencing a decision. </strong>"
+        ;
   }
 }
