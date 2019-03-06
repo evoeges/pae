29d7d223dfeeea3dd393b3d3c70f76547d87727a
evoeges <evoeges@gmail.com>
2019-03-05 22:59:19 -0500

    testing side navbar

--- js/main.js
+++ js/main.js
@@ -75,3 +75,14 @@ function updateToyAlgo(id){
      fitToSection: false
    });
  });
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
