df4f143f2cf012b0cc2ae2ea27c111073272169c
evoeges <evoeges@gmail.com>
2019-03-06 15:09:39 -0500

    started adding side navbar

--- js/main.js
+++ js/main.js
@@ -69,20 +69,11 @@ function updateToyAlgo(id){
 
  }
 
- $(document).ready(function() {
+/* $(document).ready(function() {
    $('#fullpage').fullpage({
      autoScrolling: false,
      fitToSection: false
    });
- });
+ }); */
 
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
+$('body').scrollspy({ target: '#navbar-side' })
