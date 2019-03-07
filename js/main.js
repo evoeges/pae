2e9325879b6d448c0ae6045e4fc7aae32b0d858a
evoeges <evoeges@gmail.com>
2019-03-07 15:21:24 -0500

    Revert "adding wiggly lines"
    This reverts commit e00dbe35fa58eaa389d963b918cbc69cc420847c.

--- js/main.js
+++ js/main.js
@@ -97,34 +97,3 @@ $(window).bind("scroll", function() {
         $("#navbar-side").stop().fadeOut();
     }
 });
-
-
-//wiggly line animation
-let xs = []
-for (var i = 0; i <= 500; i++) {
-  xs.push(i)
-}
-
-let t = 0
-
-function animate() {
-
-  let points = xs.map(x => {
-
-    let y = (200 + 20 * Math.sin((x + t) / 10)) -100
-
-    return [x, y]
-  })
-
-  let path = "M" + points.map(p => {
-    return p[0] + "," + p[1]
-  }).join(" L")
-
-  document.querySelector("path").setAttribute("d", path)
-
-  t += 2
-
-  requestAnimationFrame(animate)
-}
-
-animate()
