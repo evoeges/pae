b0702bb9e575c7f234cc674444f7f65035ef5b4f
evoeges <evoeges@gmail.com>
2019-04-02 15:46:51 -0400

    toy algo for mobile

--- js/ToyAlgorithm.js
+++ js/ToyAlgorithm.js
@@ -31,6 +31,7 @@ ToyAlgorithm.prototype.initVis = function() {
   vis.svg = d3.select("#" + vis.parentElement).append("svg")
     .attr("width", vis.width + vis.margin.left + vis.margin.right)
     .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+    .attr("preserveAspectRatio", "xMinYMin meet")
     .append("g")
     .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
